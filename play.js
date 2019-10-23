517c1e92d8f0711660d07639d451f3afc9e4940f
Fatima Altaf <fatima.altaf@gmail.com>
2019-10-23 18:58:31 +0000

    setting up ctrl + c command to exit

--- play.js
+++ play.js
@@ -2,6 +2,28 @@ const connect = require('./client');
 
 const net = require('net');
 
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+
+const handleUserInput = function() {
+    stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+      } 
+    });
+
+    };
+
+    handleUserInput();
+    
+    return stdin;
+  };
+
+setupInput();
+
 /**
  * Establishes connection with the game server
  */
