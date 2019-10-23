a8f75e8f7338e818e1342def42ef1441df8e6831
Fatima Altaf <fatima.altaf@gmail.com>
2019-10-23 18:33:42 +0000

    hard code for trying to move the snake up and down

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-const connect = function() {
+const connect = function() { //connecting server to client
     const conn = net.createConnection({ 
       host: '172.46.2.204',
       port: 50541   
@@ -9,12 +9,26 @@ const connect = function() {
   conn.setEncoding('utf8'); 
   conn.on('data', function(data) { //connection established
     console.log("data has come to you from server: ", data); //receiving data from server
-  })
+  });
 
   conn.on('connect', function() { //once connection established
     console.log("Successfully connected to game server"); //confirmation message print to us
     conn.write("Name: UGH"); //sending data to server
-  })
+  });
+
+
+//   conn.on('connect', function() { //once connection established
+//     conn.write("Move: left");
+//   });
+
+//   conn.on('connect', function() { //once connection established
+//     conn.write("Move: down");
+//   });
+
+//   conn.on('connect', function() { //once connection established
+//     conn.write("Move: right");
+//   });
+
 
     return conn;
   }
